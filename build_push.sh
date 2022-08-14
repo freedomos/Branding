5eb9529e81885a9a05a8d5ff85968b6b7024dae1
Erik Dubois <erik.dubois@gmail.com>
2022-08-14 22:17:17 +0100

    UPDATE

--- build_push.sh
+++ build_push.sh
@@ -18,7 +18,7 @@ echo "####################################"
 
 # Committing to the local repository with a message containing the time details and commit text
 
-git commit -m "PKGBUILD UPDATE"
+git commit -m "UPDATE"
 
 # Push the local files to github
 
